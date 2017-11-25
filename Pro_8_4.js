bba004b4975aa51c7f78b0486928a54296f15772
Sylwia <sylwia.pawelec@wp.pl>
2017-11-25 12:50:20 +0100

    rename files

--- Pro_8_4.js
+++
@@ -1,7 +0,0 @@
-// javascript
-
-/*var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);*/
-//alert ("Warinng!"); 
-pompt ("What is your name?");
---
+++ script.js
@@ -0,0 +1,3 @@
+alert ("Warinng!"); // this is a linear comment
+/*prompt ("What is your name?");
+confirm(prompt("What is your name?") + "Are you sure you want to leave?");*/
